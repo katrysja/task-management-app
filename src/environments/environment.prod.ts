bcb24e7dddd9519a2b57056bbf8ca53da162c24b
katrysja <catherine.slepchenko@gmail.com>
2022-11-07 13:29:29 +0100

    fix environment value

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    backendURL: 'https://ks-task-management-service.herokuapp.com/'
+    backendURL: 'https://ks-task-management-service.herokuapp.com'
 };
